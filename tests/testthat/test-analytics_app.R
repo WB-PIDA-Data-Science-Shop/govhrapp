457b7a16599b1fb38fd4f6e0084755692534f302
Galileu Kim <galileukim@worldbank.org>
2026-05-11 14:35:43 -0400

    fix the test

--- tests/testthat/test-analytics_app.R
+++ tests/testthat/test-analytics_app.R
@@ -1,8 +1,6 @@
 test_that("run_govhrapp creates a shiny app object", {
   # Test that the function returns a shiny.appobj
-  source(
-    system.file("R/global.R", package = "govhrapp")
-  )
+  source(testthat::test_path("../../R/global.R"))
 
   app <- run_govhrapp(personnel_data)
   
