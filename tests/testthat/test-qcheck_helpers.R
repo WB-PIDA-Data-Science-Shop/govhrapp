206f04669adc42e1c15490f3c2d723244980eac8
Galileu Kim <galileukim@worldbank.org>
2026-06-29 10:35:35 -0400

    update documentation

---
+++ tests/testthat/test-qcheck_helpers.R
@@ -0,0 +1,11 @@
+test_that("compute_coverage works correctly", {
+  data <- data.frame(
+    a = c(1, 2, NA, 4),
+    b = c(NA, 2, 3, 4)
+  )
+
+  expect_equal(
+    compute_global_coverage(data),
+    2/8
+  )
+})
