b2a2cfbda625d77e74fe18d17ba1c5ea67eecbb2
Galileu Kim <galileukim@worldbank.org>
2026-08-27 16:01:30 -0400

    begin development of an alluvial diagram

--- R/analytics_workforce_helpers.R
+++ R/analytics_workforce_helpers.R
@@ -38,3 +38,63 @@ workforce_transition_ui <- function(id, .data) {
     )
   )
 }
+
+#' Detect Career Transitions
+#' 
+#' @param .data A data frame containing workforce data.
+#' @param id_col A character string specifying the column name for the unique identifier (default is "contract_id").
+#' @param group_cols A character vector specifying the column names for grouping (e.g., paygrade, department).
+#' @param return_all A logical value indicating whether to return all records (including non-transitions) or only transitions (default is FALSE).
+#' 
+#' @importFrom data.table as.data.table setorderv rleidv
+#' @return A data frame containing detected career transitions with columns for the unique identifier, from group, to group, and reference date.
+#' 
+#' @export
+detect_career_transition <- function(
+  .data, id_col = "contract_id", group_cols,
+  return_all = FALSE
+) {
+  dt <- as.data.table(.data)
+
+  dt <- dt[complete.cases(dt[, c(id_col, group_cols), with = FALSE])]
+
+  # if necessary, combine group cols into a single column
+  if (length(group_cols) > 1) {
+    dt[, grouping := do.call(paste, c(.SD, sep = " | ")), .SDcols = group_cols]
+
+    group_cols <- "grouping"
+  }
+
+  setorderv(dt, c(id_col, "ref_date"))
+
+  # collapse to spell, i.e., when an entity stays in the same group for
+  # consecutive periods
+  dt[, .spell_id := rleidv(.SD), by = id_col, .SDcols = group_cols]
+
+  spells <- unique(dt, by = c(id_col, ".spell_id"))[
+    , c(id_col, group_cols, "ref_date"), with = FALSE
+  ]
+
+  setnames(spells, group_cols, "from")
+
+  # create a column for the ref_date of transition
+  spells[
+    , 
+    `:=`(
+      to = shift(from, type = "lead"),
+      start_ref_date = ref_date,
+      end_ref_date = shift(ref_date, type = "lead")
+    ), 
+    by = id_col
+  ]
+
+  out <- spells[,  c(id_col, "from", "to", "start_ref_date", "end_ref_date"), with = FALSE]
+  setorderv(out, c(id_col, "start_ref_date"))
+
+  # if return_all is FALSE, remove non-transitions
+  if (!return_all) {
+    out <- out[!is.na(to)]
+  }
+
+  out[]
+}
